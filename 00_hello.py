52dfbcab352d2561a032d797e85795ba46bd418c
CarolinaPerezBezares <carolinapebez@gmail.com>
2025-02-11 13:14:52 -0600

    Fichero de Hello con datos básicos de Python

---
+++ 00_hello.py
@@ -0,0 +1,52 @@
+# Esto es un comentario: 
+# Hola mundo en Python
+# Nota: Para comentarios de unas sola línea hay que utilizar el carácter de gato
+print ('Hola Python')
+
+
+"""
+
+Este es un comentario 
+en varias
+líneas
+
+"""
+
+
+"""
+Nota: Para comentarios
+de varias líneas
+se utilizan estás comillas y así poder escribir 
+en varías líneas seguídas.
+Ojo: Son tres comillas arriba y tres comillas abajo para que 
+el texto quede dentro de ellas.
+"""
+
+
+'''
+Ojo: Esto también es un comentario de
+varías líneas.
+Por qué? Python admite los dos tipos de 
+comillas.
+Cuáles? La comilla simple y la comilla doble.
+Los dos tipos de comillas sirve en las 
+impresiones/print -> (cadenas de texto).
+'''
+
+print (type(2.3)) #Imprime la clase del tipo de dato que está dentro, en este caso float.
+#Float
+
+print (type ('Hola Python'))#De igula manera impreme la clase del dato.
+#str
+
+#Ejemplos de consultas con algunos tipos de datos para entender más los prints anteriores:
+print(type('Soy un dato str'))
+print(type(5)) # Tipo 'int'
+print(type(1.5)) #Tipo 'float'
+print(type(3 + 1j)) # Tipo 'complex'
+print(type(True)) #Tipo 'boolean'
+
+print(type(print('Mi cadena de texto'))) #Tip 'NoneType', esto quiere decir que no existe esa sintaxis de print().
+#Ojo: print() es una función por lo tanto no tiene un tipo de dato ya que es una función del sistema.
+
+
